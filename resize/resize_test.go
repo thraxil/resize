b508c35c95090b82af32133f22cffbfdc0a8077a
Anders Pearson <anders@columbia.edu>
2012-05-01 22:38:05 -0400

    more DRY tests

--- resize/resize_test.go
+++ resize/resize_test.go
@@ -93,6 +93,14 @@ func Test_MakeSizeSpec(t *testing.T) {
 
 }
 
+type toRectTestCase struct {
+	Label string
+	SizeSpec *sizeSpec
+	Rect image.Rectangle
+	ExpectedWidth int
+	ExpectedHeight int
+}
+
 func Test_ToRect(t *testing.T) {
 	full_sized := MakeSizeSpec("full")
 	square_sized := MakeSizeSpec("100s")
@@ -104,149 +112,146 @@ func Test_ToRect(t *testing.T) {
 	taller_than_wide := image.Rect(0,0,500,1000)
 	square_image := image.Rect(0,0,1000,1000)
 
+	cases := []toRectTestCase{
+		{
+		Label: "full-sized should be no-op",
+		SizeSpec: full_sized,
+		Rect: square_image,
+		ExpectedWidth: square_image.Dx(),
+		ExpectedHeight: square_image.Dy(),
+		},
 
-	// full-size should be no-op
-	results := full_sized.ToRect(square_image)
-	if results.Dx() != square_image.Dx() {
-		t.Error("bad width")
-	}
-	if results.Dy() != square_image.Dy() {
-		t.Error("bad height")
-	}
+		{
+		Label: "square == square",
+		SizeSpec: square_sized,
+		Rect: square_image,
+		ExpectedWidth: square_image.Dx(),
+		ExpectedHeight: square_image.Dy(),
+		},
 
-	// square == square
-	results2 := square_sized.ToRect(square_image)
-	if results2.Dx() != square_image.Dx() {
-		t.Error("bad width")
-	}
-	if results2.Dy() != square_image.Dy() {
-		t.Error("bad height")
-	}
+		{
+		Label: "square != square (wider than taller)",
+		SizeSpec: square_sized,
+		Rect: wider_than_tall,
+		ExpectedWidth: 500,
+		ExpectedHeight: 500,
+		},
 
-	// square != square (wider than taller)
-	results3 := square_sized.ToRect(wider_than_tall)
-	if results3.Dx() != 500 {
-		t.Error("bad width")
-	}
-	if results3.Dy() != 500 {
-		t.Error("bad height")
-	}
+		{
+		Label: "square != square (taller than wide)",
+		SizeSpec: square_sized,
+		Rect: taller_than_wide,
+		ExpectedWidth: 500,
+		ExpectedHeight: 500,
+		},
 
-	// square != square (taller than wider)
-	results4 := square_sized.ToRect(taller_than_wide)
-	if results4.Dx() != 500 {
-		t.Error("bad width")
-	}
-	if results4.Dy() != 500 {
-		t.Error("bad height")
-	}
+		{
+		Label: "width constrained square image",
+		SizeSpec: width_constrained,
+		Rect: square_image,
+		ExpectedWidth: 1000,
+		ExpectedHeight: 1000,
+		},
 
-	// width constrained square image
-	results5 := width_constrained.ToRect(square_image)
-	if results5.Dx() != 1000 {
-		t.Error("bad width")
-	}
-	if results5.Dy() != 1000 {
-		t.Error("bad height")
-	}
+		{
+		Label: "width constrained wider than tall",
+		SizeSpec: width_constrained,
+		Rect: wider_than_tall,
+		ExpectedWidth: 1000,
+		ExpectedHeight: 500,
+		},
 
-	// width constrained wider than tall
-	results6 := width_constrained.ToRect(wider_than_tall)
-	if results6.Dx() != 1000 {
-		t.Error("bad width")
-	}
-	if results6.Dy() != 500 {
-		t.Error("bad height")
-	}
+		{
+		Label: "width constrained taller than wide",
+		SizeSpec: width_constrained,
+		Rect: taller_than_wide,
+		ExpectedWidth: 500,
+		ExpectedHeight: 1000,
+		},
 
-	// width constrained taller than wide
-	results7 := width_constrained.ToRect(taller_than_wide)
-	if results7.Dx() != 500 {
-		t.Error("bad width")
-	}
-	if results7.Dy() != 1000 {
-		t.Error("bad height")
-	}
+		{
+		Label: "height constrained square image",
+		SizeSpec: height_constrained,
+		Rect: square_image,
+		ExpectedWidth: 1000,
+		ExpectedHeight: 1000,
+		},
 
-	// height constrained square image
-	results8 := height_constrained.ToRect(square_image)
-	if results8.Dx() != 1000 {
-		t.Error("bad width")
-	}
-	if results8.Dy() != 1000 {
-		t.Error("bad height")
-	}
+		{
+		Label: "height constrained wider than tall",
+		SizeSpec: height_constrained,
+		Rect: wider_than_tall,
+		ExpectedWidth: 1000,
+		ExpectedHeight: 500,
+		},
 
-	// height constrained wider than tall
-	results9 := height_constrained.ToRect(wider_than_tall)
-	if results9.Dx() != 1000 {
-		t.Error("bad width")
-	}
-	if results9.Dy() != 500 {
-		t.Error("bad height")
-	}
+		{
+		Label: "height constrained taller than wide",
+		SizeSpec: height_constrained,
+		Rect: taller_than_wide,
+		ExpectedWidth: 500,
+		ExpectedHeight: 1000,
+		},
 
-	// height constrained taller than wide
-	results10 := height_constrained.ToRect(taller_than_wide)
-	if results10.Dx() != 500 {
-		t.Error("bad width")
-	}
-	if results10.Dy() != 1000 {
-		t.Error("bad height")
-	}
+		{
+		Label: "height and width constrained (w>h) square",
+		SizeSpec: height_and_width_constrained_wh,
+		Rect: square_image,
+		ExpectedWidth: 1000,
+		ExpectedHeight: 500,
+		},
 
-	// height and width constrained (w>h) square
-	results11 := height_and_width_constrained_wh.ToRect(square_image)
-	if results11.Dx() != 1000 {
-		t.Error("bad width")
-	}
-	if results11.Dy() != 500 {
-		t.Error("bad height")
-	}
+		{
+		Label: "height and width constrained (w>h) taller than wide",
+		SizeSpec: height_and_width_constrained_wh,
+		Rect: taller_than_wide,
+		ExpectedWidth: 500,
+		ExpectedHeight: 250,
+		},
 
-	// height and width constrained (w>h) taller than wide
-	results12 := height_and_width_constrained_wh.ToRect(taller_than_wide)
-	if results12.Dx() != 500 {
-		t.Error("bad width")
-	}
-	if results12.Dy() != 250 {
-		t.Error("bad height")
-	}
+		{
+		Label: "height and width constrained (w>h) wider than tall",
+		SizeSpec: height_and_width_constrained_wh,
+		Rect: wider_than_tall,
+		ExpectedWidth: 500,
+		ExpectedHeight: 1000,
+		},
 
-	// height and width constrained (w>h) wider than tall
-	results13 := height_and_width_constrained_wh.ToRect(wider_than_tall)
-	if results13.Dx() != 500 {
-		t.Error("bad width")
-	}
-	if results13.Dy() != 1000 {
-		t.Error("bad height")
-	}
+		{
+		Label: "height and width constrained (h>w) square",
+		SizeSpec: height_and_width_constrained_hw,
+		Rect: square_image,
+		ExpectedWidth: 500,
+		ExpectedHeight: 1000,
+		},
 
-	// height and width constrained (h>w) square
-	results14 := height_and_width_constrained_hw.ToRect(square_image)
-	if results14.Dx() != 500 {
-		t.Error("bad width")
-	}
-	if results14.Dy() != 1000 {
-		t.Error("bad height")
-	}
+		{
+		Label: "height and width constrained (h>w) taller than wide",
+		SizeSpec: height_and_width_constrained_hw,
+		Rect: taller_than_wide,
+		ExpectedWidth: 500,
+		ExpectedHeight: 1000,
+		},
 
-	// height and width constrained (h>w) taller than wide
-	results15 := height_and_width_constrained_hw.ToRect(taller_than_wide)
-	if results15.Dx() != 500 {
-		t.Error("bad width")
-	}
-	if results15.Dy() != 1000 {
-		t.Error("bad height")
+		{
+		Label: "height and width constrained (h>w) wider than tall",
+		SizeSpec: height_and_width_constrained_hw,
+		Rect: wider_than_tall,
+		ExpectedWidth: 500,
+		ExpectedHeight: 1000,
+		},
+
 	}
 
-	// height and width constrained (h>w) wider than tall
-	results16 := height_and_width_constrained_hw.ToRect(wider_than_tall)
-	if results16.Dx() != 500 {
-		t.Error("bad width")
-	}
-	if results16.Dy() != 1000 {
-		t.Error("bad height")
+	for i := range cases {
+		c := cases[i]
+		r := c.SizeSpec.ToRect(c.Rect)
+		if r.Dx() != c.ExpectedWidth {
+			t.Error(c.Label, "-- bad width",r.Dx()," expected",c.ExpectedWidth)
+		}
+		if r.Dy() != c.ExpectedHeight {
+			t.Error(c.Label, "-- bad height",r.Dy()," expected",c.ExpectedHeight)
+		}
 	}
 
 }
